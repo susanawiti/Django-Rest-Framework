fbee8421352d768ade52d241745ec470c9150e9a
susanawiti <susanawiti95@gmail.com>
2017-09-15 17:09:15 +0300

    Update models.py

--- restwork/models.py
+++ restwork/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    names = models.CharField(max_length = 100)
+    course = models.CharField(max_length = 50)
+    description = models.TextField(default='Good student')
+    registration_date = models.DateField(default=datetime.date.today())
+    graduation_date = models.DateField(default = datetime.date.today)
